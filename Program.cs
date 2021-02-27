6b256eaae1480708dd8162d7acc23b608d20d9b2
Gloomy-stack <karl.yotl@gmail.com>
2021-02-27 14:14:10 +0100

    Added main code

--- Program.cs
+++ Program.cs
@@ -10,6 +10,36 @@ namespace Parzystosc_liczb
     {
         static void Main(string[] args)
         {
+            int number;
+            Console.WriteLine("0 kończy działanie programu");
+            try
+            {
+                do
+                {
+                    Console.Write("Podaj liczbę:");
+                    number = GetNumber();
+                    if (number % 2 == 0)
+                        Console.WriteLine("Liczba parzysta");
+                    else
+                        Console.WriteLine("Liczba nieparzysta");
+
+                } while (number != 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+
+        public static int GetNumber()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int number))
+                throw new Exception("Podaj prawidłową liczbę.");
+            return number;
         }
     }
 }
